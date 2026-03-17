9a49ffe54581e8ff088c61488ae85eb9746aac70
balconyRewrap <110861634+balconyRewrap@users.noreply.github.com>
2026-03-17 08:46:53 +0100

    feat: added health_check endpoint

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 from apps.books.views import (
